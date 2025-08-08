bba8338567f6d3667f1e4ce22e40c436edc9cd87
Jahan <you@example.com>
2025-08-08 12:31:23 +0330

    start project and configure TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'lateef-regular':'lateef-regular',
+      'lateef-bold': 'lateef-bold'
+    },
+    screens : {
+      'sm':'480px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
