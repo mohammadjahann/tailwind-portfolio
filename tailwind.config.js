30ecc9d4ef193b624ac840e958ca14bd2db0c891
Jahan <you@example.com>
2025-08-12 21:27:57 +0330

    Define and style "About Me" section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,38 @@
- /** @type {import('tailwindcss').Config} */
- export default {
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
     
-    extend: {
-      fontFamily:{
-        'lateef-regular':'lateef-regular',
-        'lateef-bold': 'lateef-bold',
-        'Caprasimo-Regular' : 'Caprasimo-Regular'
+    container: {
+      center: true, // وسط‌چین شدن کانتینر
+      padding: {
+        DEFAULT: '1rem', // پدینگ پیش‌فرض
+        sm: '2rem',
+        md: '3rem',
+        lg: '4rem',
+        xl: '5rem',
       },
-      screens : {
-        'sm':'480px',
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'lateef-regular': 'lateef-regular',
+        'lateef-bold': 'lateef-bold',
+        'Caprasimo-Regular': 'Caprasimo-Regular'
+      },
+      screens: {
+        'sm': '480px',
         'lg': '1024px',
         'md': '768px'
       },
-      spacing:{
-        'big':'38rem'
+      spacing: {
+        'big': '38rem'
       },
     },
   },
