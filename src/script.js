2016319e00da5fb20421f781a2189531208229cc
Jahan <you@example.com>
2025-08-15 00:47:29 +0330

    Define Scroll behavior on nav links

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
 const hamburger = document.querySelector('#hamburger')
 const menu =document.querySelector('#menu')
+const hlinks = document .querySelectorAll('#hlink')
 
 hamburger.addEventListener('click',(event)=>{
     menu.classList.toggle('hidden')
     hamburger.classList.toggle('bg-white')
     
+})
+
+hlinks.forEach(link=>{
+    link.addEventListener('click',(event)=>{
+        menu.classList.toggle('hidden')
+        
+    })
 })
